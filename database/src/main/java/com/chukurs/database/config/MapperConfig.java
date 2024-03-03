103093be33ce314aab94a780df75c2025992aae0
mc <m.cukurs1997@gmail.com>
2024-03-03 14:31:48 +0200

    enable-nested-objects

--- database/src/main/java/com/chukurs/database/config/MapperConfig.java
+++ database/src/main/java/com/chukurs/database/config/MapperConfig.java
@@ -2,6 +2,8 @@ package com.chukurs.database.config;
 
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +13,9 @@ public class MapperConfig {
     //it is available in context as its a BEAN
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
     }
 
 }
